000ffe8adb71c2508fa5a4ab301dd6e7185da896
Matija Nalis <mnalis-git@voyager.hr>
2023-04-25 03:39:22 +0200

    rewrite qick & dirty & buggy implementation of manual XML parsing (just for faster testing!)

--- myosmnotes_parser.pl
+++ myosmnotes_parser.pl
@@ -2,7 +2,8 @@
 # Matija Nalis <mnalis-git-openstreetmap@voyager.hr> GPLv3+ started 20150201
 # parses OSM notes planet dump into BerkeleyDB
 #
-# WARNING: it seems to segault with libxml-sax-expatxs-perl installed on Debian Bullseye
+# WARNING: this branch users unsafe, but FAST parser (e.g. 20 seconds instead of 8 minutes!)
+# FIXME: might fail to parse multiline comments and will break if syntax of XML changes even slightly so beware! Use for fast develop testing only! And be buggy otherwise...
 
 use strict;
 use warnings;
@@ -11,7 +12,6 @@ use feature 'say';
 
 use DB_File;
 use DBM_Filter;
-use XML::SAX;
 
 $ENV{'PATH'} = '/usr/bin:/bin';
 my $OSN_FILE = 'OK.planet-notes-latest.osn.bz2';
@@ -25,22 +25,18 @@ my $DECOMPRESSOR = 'bzcat';
 if (-e '/usr/bin/pbzip2') { $DECOMPRESSOR = 'pbzip2 -dc' }  # use faster decompressing if available
 
 
-#$| = 1;
+#$| = 1; # FIXME only enable for debug to avoid interleaved STDOUT/STDERR
 my $start_time = time;
 print 'parsing... ';
 
 open (my $xml_file, '-|', "$DECOMPRESSOR $OSN_FILE");
-#binmode $xml_file, ":encoding(UTF-8)";	# FIXME: segfaults on perl 5.18.2-2ubuntu1.3 on Ubuntu 14.04.5 LTS
+#open my $xml_file, '<', 'a';
+binmode $xml_file, ":encoding(UTF-8)";	# FIXME: segfaults on perl 5.18.2-2ubuntu1.3 on Ubuntu 14.04.5 LTS when using SAX?
 binmode STDOUT, ":encoding(UTF-8)";
 binmode STDERR, ":encoding(UTF-8)";
 
 print 'opened bz2... ';
 
-my $parser = XML::SAX::ParserFactory->parser(
-  Handler => SAX_OSM_Notes->new
-);
-
-print 'handler... ';
 # case insensitive compare of hash keys
 sub db_compare {
     my($key1, $key2) = @_;
@@ -59,7 +55,7 @@ my $db_note = tie my %NOTE, "DB_File", "$DB_NOTES_FILE_TMP";
 $db_note->Filter_Value_Push('utf8');
 
 print 'go... ';
-$parser->parse_file($xml_file);
+parse_file($xml_file);
 
 undef $db_user;
 undef $db_note;
@@ -77,111 +73,117 @@ exit 0;
 
 
 
-#########################################
-######### SAX parser below ##############
-#########################################
+###################################################
+######### QUICK & DIRTY parser below ##############
+###################################################
 
-package SAX_OSM_Notes;
-use base qw(XML::SAX::Base);
-
-use strict;
-use warnings;
-
-#use Data::Dumper;
-# when a '<foo>' is seen
-sub start_element
+sub parse_file
 {
-   my $this = shift;
-   my $tag = shift;
+   my ($xml_file) = @_;
+   my $this = {};
+   $this->{'inside_comment'} = 0;
    
-   #print ".";
+   while (my $line = <$xml_file>) {
+   
+    #print "DEBUG1: parsing line: $line";
 
-   if ($tag->{'LocalName'} eq 'note') {
-     my $note_id = $tag->{'Attributes'}{'{}id'}{'Value'};
-     my $created_at = $tag->{'Attributes'}{'{}created_at'}{'Value'};
-     #say "\n/mn/ start note_id=$note_id";
-     $this->{'note_ID'} = $note_id;
-     $this->{'first_text'} = undef;
-     $this->{'last_date'} = $created_at;
-     %{$this->{'users'}} = ();
-     #Dumper($tag->{Attributes})
-   }
+    # empty lines
+    if ($line =~ /^\s*$/) { next };
+    # <?xml>
+    if ($line =~ /^\s*<\?xml.*\?>\s*$/) { next };
+
+    # <osm-notes>, </osm-notes>
+    if ($line =~ /^\s*<\/?osm-notes>\s*$/) { next };
+    
+    # <note ... />
+    if ($line =~ /^\s*<note id.*\/>\s*$/) { next };    # skip empty notes without sub-tags
    
-   if ($tag->{'LocalName'} eq 'comment') {
-     my $user_id = $tag->{'Attributes'}{'{}user'}{'Value'};
-     my $action = $tag->{'Attributes'}{'{}action'}{'Value'};
-     $this->{'last_action'} = $1 if $action =~ /^(?:re)?(opened|closed)$/;
-     $this->{'text'} = '';  # start with empty string, we'll fill it later in characters()
-     $this->{'last_comment_is_resurvey'} = 0;   # every new comment resets it to 0 as it does NOT contain "#surveyme"  at this time (as "text" is empty)
-     if (defined($user_id)) {
-       #say "  comment by user_id=$user_id, note_id=" . $this->{'note_ID'};
-       $this->{'users'}{$user_id} = 1;
+    # <note>     
+    if ($line =~ s/^\s*<note id="(\d+?)".*created_at="(.+?)">\s*$//) {
+       my $note_id = $1;
+       my $created_at = $2;
+       $this->{'inside_comment'} = 0;
+       #say "\n/mn/ start note_id=$note_id";
+       $this->{'note_ID'} = $note_id;
+       $this->{'first_text'} = undef;
+       $this->{'last_date'} = $created_at;
+       %{$this->{'users'}} = ();
      }
-     $this->{'last_date'} = $tag->{'Attributes'}{'{}timestamp'}{'Value'};
-     #say '   comment timestamp: '  . $this->{'last_date'};
-   }
-   
-   # call the super class to properly handle the event
-   return $this->SUPER::start_element($tag)
-}
 
-# content of a element (stuff between <foo> and </foo>) - may be multiple, so concat() it!
-sub characters
-{
-   my $this = shift;
-   my $tag = shift;
-   $this->{'text'} .= $tag->{'Data'};
-}
-
-# when a '</foo>' is seen
-sub end_element
-{
-   my $this = shift;
-   my $tag = shift;
-
-   if ($tag->{LocalName} eq 'note') {
-     if ($this->{'last_action'} eq 'opened') {	# we're only interested in (re-)opened notes!
-        #if ($count++ > 999) { say "exiting on $count for DEBUG, FIXME"; exit 0;} ; print "[$count] ";
-        #say "end_note (non-closed), last note_id=" . $this->{'note_ID'} . ", first_text=" . $this->{'first_text'} . ", last_date=" . $this->{'last_date'};
-        #print '.';
-	#warn "no last_date for id=" . $this->{'note_ID'} if !defined $this->{'last_date'};
-	#warn "no first_text for id=" . $this->{'note_ID'} . ' date=' . $this->{'last_date'} if !defined $this->{'first_text'};
-        $NOTE{$this->{'note_ID'}} = $this->{'last_comment_is_resurvey'} . ' ' . $this->{'last_date'} . ' ' . ($this->{'first_text'} || ' ');	# save it to database
-        $this->{'first_text'} = 1;	# reduce memory usage (no need to keep full text in memory, but still evalute to true)
-        
-        foreach my $username (keys %{$this->{'users'}}) {
-            #say "\tuser=$username -- note is opened, remember it!";
-            if (defined($USER{$username})) {
-               $USER{$username} .= ' ' . $this->{'note_ID'};
-            } else {
-               $USER{$username} = $this->{'note_ID'};
-            }
-        }
+     #print "DEBUG2: parsing line: $line";
+     # <comment>
+     if ($line =~ s/^\s*<comment action="(.+?)".*?timestamp="(.+?)"(?: .*?user="(.+?)")?.*?>//) {
+       my $action = $1;
+       my $timestamp = $2;
+       my $user_id = $3;
+       $this->{'inside_comment'} = 1;
+       $this->{'last_action'} = $1 if $action =~ /^(?:re)?(opened|closed)\s*$/;
+       $this->{'text'} = '';  # start with empty string, we'll fill it later in characters()
+       $this->{'last_comment_is_resurvey'} = 0;   # every new comment resets it to 0 as it does NOT contain "#surveyme"  at this time (as "text" is empty)
+       if (defined($user_id)) {
+         #say "  comment ($action) by user_id=$user_id, note_id=" . $this->{'note_ID'};
+         $this->{'users'}{$user_id} = 1;
+       } else { 
+         #say  "no user specified"
+       }
+       $this->{'last_date'} = $timestamp;
+       #say '   comment timestamp: '  . $this->{'last_date'};
+       #use Data::Dumper; say Dumper($this);
      }
-   }
+
+     #print "DEBUG3: parsing line (inside_comment=" . $this->{'inside_comment'} . "): $line";
+     # concat text between tags
+     if ($this->{'inside_comment'} and $line =~ s/^\s*([^<]*?)\s*($|<)/$2/) {
+       #say  '     adding >' . $1 . '< to text: >' . $this->{'text'} . '<';
+       $this->{'text'} .= $1;            
+     }
+     
+     #print "DEBUG4: parsing line: $line";
+     # </comment>
+     if ($line =~ s/^\s*<\/comment>\s*$//) {
+       $this->{'inside_comment'} = 0;
+       my $current_text = $this->{'text'};
+       if ($current_text =~ /#surveyme/i) {
+           $this->{'last_comment_is_resurvey'} = 1;
+       }
+       #say 'end_comment[' . $this->{'note_ID'} .  '], surveyme=' . $this->{'last_comment_is_resurvey'} . ' full current text=' . $current_text;	# full text of this comment
+       if (!defined($this->{'first_text'})) {	# only the full text of the FIRST comment (opening of bug)
+           $this->{'first_text'} = $current_text;
+           $this->{'first_text'} =~ s/\s+/ /g;
+       }
+       #say "comment tag=" . Dumper($tag);
+     }
+
+     #print "DEBUG5: parsing line: $line";
+     # </note>
+     if ($line =~ s/^\s*<\/note>\s*$//) {
+       #use Data::Dumper; say Dumper($this);
+       if (defined($this->{'last_action'}) and $this->{'last_action'} eq 'opened') {	# we're only interested in (re-)opened notes!
+          #if ($count++ > 999) { say "exiting on $count for DEBUG, FIXME"; exit 0;} ; print "[$count] ";
+          #say "end_note (non-closed), last note_id=" . $this->{'note_ID'} . ", first_text=" . $this->{'first_text'} . ", last_date=" . $this->{'last_date'};
+          #print '.';
+          #warn "no last_date for id=" . $this->{'note_ID'} if !defined $this->{'last_date'};
+          #warn "no first_text for id=" . $this->{'note_ID'} . ' date=' . $this->{'last_date'} if !defined $this->{'first_text'};
+          $NOTE{$this->{'note_ID'}} = $this->{'last_comment_is_resurvey'} . ' ' . $this->{'last_date'} . ' ' . ($this->{'first_text'} || ' ');	# save it to database
+          $this->{'first_text'} = 1;	# reduce memory usage (no need to keep full text in memory, but still evalute to true)
+          
+          foreach my $username (keys %{$this->{'users'}}) {
+              #say "\tuser=$username -- note is opened, remember it!";
+              if (defined($USER{$username})) {
+                 $USER{$username} .= ' ' . $this->{'note_ID'};
+              } else {
+                 $USER{$username} = $this->{'note_ID'};
+              }
+          }
+       }
+       $this = {};
+     }
+
+     if ($line) {
+       die "line should be empty by now, but it is: $line";
+     }
+     
+   }                                                            # end while
    
-  if ($tag->{'LocalName'} eq 'comment') {
-    my $current_text = $this->{'text'};
-    if ($current_text =~ /#surveyme/i) {
-        $this->{'last_comment_is_resurvey'} = 1;
-    }
-    #say 'end_comment[' . $this->{'note_ID'} .  '], surveyme=' . $this->{'last_comment_is_resurvey'} . ' full current text=' . $current_text;	# full text of this comment
-    if (!defined($this->{'first_text'})) {	# only the full text of the FIRST comment (opening of bug)
-        $this->{'first_text'} = $current_text;
-        $this->{'first_text'} =~ s/\s+/ /g;
-    }
-    #say "comment tag=" . Dumper($tag);
-  }
-      
-   return $this->SUPER::end_element($tag)
+   
 }
-
-sub start_document
-{
-   my $this = shift;
-   my $tag = shift;
-   say "start document";
-   return $this->SUPER::start_document($tag)
-}
-
-1;
